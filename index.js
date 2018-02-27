e2d39311d95a651306d0e2a24802c4ce621867aa
agj <ale@agj.cl>
2018-02-26 23:24:08 -0300

    Pinyin column reduced to a single reading, taking the last value, which, according to the Unihan documentation, is the preferred for traditional writing. Sound column also reflects this change.

--- index.js
+++ index.js
@@ -10,14 +10,14 @@ const toStringEntry = o => [
 		o.studyOrder,
 		o.conflated ? o.conflated.join('') : '',
 		o.simplified ? o.simplified.join('') : '',
-		o.pinyin,
+		o.pinyin ? o.pinyin.split(' ').into(R.last) : '',
 		o.heisigKeyword,
 		o.meaning,
 		o.vocabulary.map(R.prop('word')).join(' '),
 		o.vocabulary.map(R.prop('pinyin')).join(' '),
 		o.japaneseKun,
 		o.japaneseOn,
-		'[sound:agj-pinyin-' + pinyinToFile(o.pinyin) + '.mp3]',
+		'[sound:agj-pinyin-' + pinyinToFile(o.pinyin ? o.pinyin.split(' ').into(R.last) : '') + '.mp3]',
 		o.frequencyRank
 	].join('\t');
 const pinyinToFile = py => {
