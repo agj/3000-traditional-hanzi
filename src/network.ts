c883e9b967859dbd9a7add96f9246b3826eff6ee
agj <ale@agj.cl>
2026-03-28 01:43:53 -0300

    Migrate network code to TS

---
+++ src/network.ts
@@ -0,0 +1,110 @@
+import "dot-into";
+import {
+  concat,
+  contains,
+  filter,
+  has,
+  map,
+  mergeWith,
+  pipe,
+  replace,
+  split,
+  trim,
+  uniq,
+} from "ramda";
+import fs from "fs";
+import xre from "xregexp";
+
+type Decomposition = { character: string; decomposition: string[] };
+
+const getFile = (filename: string) =>
+  fs.readFileSync(filename, "utf-8").split("\n").filter(notEmptyLine);
+const notEmptyLine = pipe(
+  trim,
+  (line) => line.length > 0 && line[0] !== "#" && !/^\/\*/.test(line),
+);
+const stripNonHan = replace(xre("\\P{Han}", "gA"), "");
+const getIdsFile = (
+  filename: string,
+  preprocess: (s: string[]) => [string, string],
+) =>
+  getFile(filename)
+    .map(split("\t"))
+    .map(preprocess)
+    .reduce((obj: Record<string, Decomposition>, [char, dec]) => {
+      obj[char] = {
+        character: char,
+        decomposition: stripNonHan(dec)
+          .split("")
+          .filter((c) => c !== char)
+          .into((v) => uniq(v)),
+      };
+      return obj;
+    }, {});
+
+const ids = getIdsFile("data/external/ids.txt", ([_, char, ...decs]) => {
+  if (!char) {
+    throw new Error("IDs file line has wrong format");
+  }
+  return [char, decs.reduce(concat, "")];
+});
+const wrongAnalysisCats = ["簡体", "或字"];
+const idsAnalysis = getIdsFile(
+  "data/external/ids-analysis.txt",
+  ([_, char, dec, cat]) => {
+    if (!char || !dec) {
+      throw new Error("IDs analysis file line has wrong format");
+    }
+    return [char, contains(cat, wrongAnalysisCats) ? "" : dec];
+  },
+);
+const reCjkDecomp = /^(.+):.+\((.+)\)$/u;
+const redefineNumeric =
+  (mapping: Record<string, Decomposition>) =>
+  (component: string): string[] =>
+    /\d+/.test(component)
+      ? (mapping[component]?.decomposition
+          .flatMap(redefineNumeric(mapping))
+          .into((v) => uniq(v)) ?? [])
+      : [component];
+const cjkDecomp = getFile("data/external/cjk-decomp.txt")
+  .map((l): [string, string[]] => [
+    l.replace(reCjkDecomp, "$1"),
+    l.replace(reCjkDecomp, "$2").split(","),
+  ])
+  .reduce((obj: Record<string, Decomposition>, [char, dec]) => {
+    obj[char] = { character: char, decomposition: dec };
+    return obj;
+  }, {})
+  .into((all: Record<string, Decomposition>) =>
+    all
+      .into(filter((o: Decomposition) => !/\d+/.test(o.character)))
+      .into((decompositions) =>
+        map(
+          (o: Decomposition): Decomposition => ({
+            character: o.character,
+            decomposition: o.decomposition
+              .flatMap(redefineNumeric(all))
+              .into((v) => uniq(v)),
+          }),
+          decompositions,
+        ),
+      ),
+  );
+
+const mergeDecompositions = (a: Decomposition, b: Decomposition) => ({
+  character: a.character,
+  decomposition: concat(a.decomposition, b.decomposition).into(uniq),
+});
+const network = mergeWith(mergeDecompositions, ids, idsAnalysis)
+  .into(mergeWith(mergeDecompositions, cjkDecomp))
+  .into(
+    map((c: Decomposition) => ({
+      character: c.character,
+      decomposition: c.decomposition.filter(
+        (c) => has(c, ids) || has(c, idsAnalysis) || has(c, cjkDecomp),
+      ),
+    })),
+  );
+
+export default network;
