35f0b0549b8c28385e5e2cfb0e7f91cb9e3430f2
agj <ale@agj.cl>
2026-03-29 20:37:26 -0300

    Document utilities and remove unused

--- src/utilities.ts
+++ src/utilities.ts
@@ -2,17 +2,25 @@ import { tap, pipe, trim, replace } from "ramda";
 import fs from "fs";
 import xre from "xregexp";
 
+/**
+ * Prints a value and returns it.
+ */
 export const log = tap(console.log);
 
+/**
+ * Checks if a line of a data file is empty or is a comment.
+ */
 const notEmptyLine = pipe(
   trim,
   (line) => line.length > 0 && line[0] !== "#" && !/^\/\*/.test(line),
 );
 
+/**
+ * Reads a text file by its filename, and returns it split into lines. It gets
+ * rid of empty and comment lines.
+ */
 export const getFile = (filename: string): string[] =>
   fs.readFileSync(filename, "utf-8").split("\n").filter(notEmptyLine);
-
-export const whenAll = Promise.all.bind(Promise);
 
 /**
  * Strips all non-hanzi content from a string.
